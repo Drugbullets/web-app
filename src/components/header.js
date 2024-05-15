5f4e10686e656f9d68cfe8968162ddf9279aedf0
Drugbullets <82928973+Drugbullets@users.noreply.github.com>
2024-05-15 12:55:36 +0530

    made achange

---
+++ src/components/header.js
@@ -0,0 +1,21 @@
+import ReactDOM from 'react-dom'
+function Message (props)  {
+	{
+		return 
+		<h2>Component Example</h2>;
+
+	}
+	function App()
+	{
+		return(
+		<div>
+		<Message username="john"/>
+<Message username="selina"/>
+<Message username="arin"/>
+</div>
+
+)
+
+	}
+}
+ReactDOM.render(<MyComponent/>,documentGetElementById('root'));
